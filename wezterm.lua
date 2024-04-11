9414cee1109710c7426fcd705410e3fabd1c6e61
Marius Stern <marius-stern@gmx.de>
2024-04-11 19:35:34 +0200

    added keyboard shortcut to toggle transparent background

--- wezterm.lua
+++ wezterm.lua
@@ -11,8 +11,11 @@ wezterm.on('gui-startup', function(window)
   gui_window:perform_action(wezterm.action.ToggleFullScreen, pane)
 end)
 
+
+local background_opacity = 0.85
+
 config.hide_tab_bar_if_only_one_tab = true
-config.window_background_opacity = 0.85
+config.window_background_opacity = background_opacity 
 config.audible_bell = "Disabled"
 config.font = wezterm.font('JetBrains Mono', { italic = false })
 config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
@@ -24,5 +27,29 @@ config.colors = {
     cursor_bg = 'lightgray'
 }
 
+config.window_background_opacity = background_opacity
+
+-- toggle function
+wezterm.on("toggle-opacity", function(window, pane)
+    local overrides = window:get_config_overrides() or {}
+    if not overrides.window_background_opacity then
+        -- if no override is setup, override the default opacity value with 1.0
+        overrides.window_background_opacity = 1.0
+    else
+        -- if there is an override, make it nil so the opacity goes back to the default
+        overrides.window_background_opacity = nil
+    end
+    window:set_config_overrides(overrides)
+end)
+
+config.keys = {
+
+-- Keyboard shortcut to toggle background opacity
+    {
+        key = 'b',
+        mods = 'CTRL',
+        action = wezterm.action.EmitEvent("toggle-opacity"),
+    }
+}
 -- and finally, return the configuration to wezterm
 return config
